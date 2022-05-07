55a101748c3fb1d973e92e321d4553cfc7e6d313
motoralex73 <mo1or@mail.ru>
2022-05-07 18:13:16 +0200

    sweater lesson=2

--- sweater/src/main/java/com/example/sweater/GreetingController.java
+++ sweater/src/main/java/com/example/sweater/GreetingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -32,10 +33,26 @@ public class GreetingController {
 
     @PostMapping
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        //RequestParam - выдергивает параметры либо с формы, либо с полей ввода
         Message message = new Message(text,tag);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            System.out.println("filter != null && !filter.isEmpty()");
+            messages = messageRepo.findByTag(filter);
+        } else {
+            System.out.println("else filter");
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
         return "main";
     }
 
