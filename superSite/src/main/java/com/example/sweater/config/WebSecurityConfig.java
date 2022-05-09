1350fc77e8febd718e2e1329cf688ca14b21541e
motoralex73 <mo1or@mail.ru>
2022-05-09 19:11:54 +0200

    superSite have error

--- superSite/src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ superSite/src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+import javax.sql.DataSource;
+
 //класс, в котором происходит авторизация
 @Configuration
 @EnableWebSecurity
--- superSite/src/main/java/com/example/sweater/controller/MainController.java
+++ superSite/src/main/java/com/example/sweater/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,9 +24,20 @@ public class MainController {
         return "greeting";
     }
     @GetMapping("/main")
-    public String main(Map<String,Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages",messages);
+
+        if (filter != null && !filter.isEmpty()) {
+            System.out.println("filter != null && !filter.isEmpty()");
+            messages = messageRepo.findByTag(filter);
+        } else {
+            System.out.println("else filter");
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages",messages);
+        model.addAttribute("filter",filter);
+
         return "main";
     }
 
@@ -39,21 +51,6 @@ public class MainController {
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages",messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            System.out.println("filter != null && !filter.isEmpty()");
-            messages = messageRepo.findByTag(filter);
-        } else {
-            System.out.println("else filter");
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
         return "main";
     }
 
