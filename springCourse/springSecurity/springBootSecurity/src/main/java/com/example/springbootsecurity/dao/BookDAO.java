4722607f946bfd8cd9678af085c7ffadb78ade09
alex <mo1or@mail.ru>
2023-01-12 21:38:32 +0400

    Alishev courses spring security

---
+++ springCourse/springSecurity/springBootSecurity/src/main/java/com/example/springbootsecurity/dao/BookDAO.java
@@ -0,0 +1,8 @@
+package com.example.springbootsecurity.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDAO {
+    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+}
---
+++ springCourse/springSecurity/springBootSecurityTwo/src/main/java/com/example/springbootsecurity/controllers/StartController.java
@@ -0,0 +1,23 @@
+package com.example.springbootsecurity.controllers;
+
+import com.example.springbootsecurity.security.PersonDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+    @GetMapping()
+    public String startPage() {
+        return "index";
+    }
+
+    @GetMapping("/show")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+        return "hello";
+    }
+}
